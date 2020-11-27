7032dc27b582bb2317c43ab54f6f3cb207b6b982
xusyok <190544087@qq.com>
2020-11-27 13:34:43 +0800

    第一次实验 vuex

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 11.27vuex  引用
+import store from './store/store'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/praject/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="state">
+    <h2>{{ count }}</h2>
+    <button @click="add">+ADD</button>
+  </div>
+</template>
+
+<script>
+  import store from '@/store/store.js'
+    export default {
+        name: "state",
+      computed: {
+        count () {
+          return store.state.count
+        }
+      },
+      methods: {
+        add () {
+          store.state.count++
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
